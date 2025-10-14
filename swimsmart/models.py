cc59c6991d5edb4f8014bbdddd1ec052f909ba51
RossBurns98 <196393235+RossBurns98@users.noreply.github.com>
2025-10-14 20:34:06 +0100

    changing silly mistake.

--- swimsmart/models.py
+++ swimsmart/models.py
@@ -33,7 +33,7 @@ class UserRole(str, enum.Enum):
 
 class User(Base):
     __tablename__ = "users"
-    username = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=True)
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, index=True, nullable=False)
     password_hash = Column(String, nullable=False)
