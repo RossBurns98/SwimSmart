121d1289626815f5c86d4c4b77b98a68309c5604
RossBurns98 <196393235+RossBurns98@users.noreply.github.com>
2025-10-14 20:49:13 +0100

    update .

--- swimsmart/api/main.py
+++ swimsmart/api/main.py
@@ -66,7 +66,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://swimsmart.netlify.app/login"],   # env-configurable; defaults to ["*"]
+        allow_origins=["https://swimsmart.netlify.app"],
         allow_methods=["*"],
         allow_headers=["*"],
         allow_credentials=False,
